5d9c749fb714838df72c28832a05a8e7a7a65594
rajdhan <rajdhanpatel9999@gmail.com>
2022-10-08 23:34:12 +0530

    changes in object

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,23 +1,24 @@
 package com.driver;
 
 public class Main {
-  public static void main(String args[]){
+    public static void main(String args[]) {
+        A a = new A();
+        System.out.println(a.meth());
         B b = new B();
-        String s = b.meth();
-        System.out.println(s);
+        System.out.println(b.meth());
+
   }
   public static class A{
       public String meth(){
+//          System.out.println("parent classs");
           return "Invoking method from class A";
       }
   }
   public static class B extends A{
-      B(){
-          super();
-      }
+
         public String meth(){
 
             return "Method is overridden in Extendend class B";
         }
-  }
+    }
 }
